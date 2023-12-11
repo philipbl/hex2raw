d6a312e9dfc3278a1587f8b07f72821dba1ee4d9
Philip Lundrigan <lundrigan@byu.edu>
2023-12-11 09:07:04 -0700

    Add more tests

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import secrets
 from subprocess import run, PIPE
 
 import pytest
@@ -12,11 +13,22 @@ def get_test_files(path):
     return sorted(list(path.glob("*.txt")))
 
 
-def run_commands(test_file):
+def get_test_data(num_tests, len_test):
+    for i in range(num_tests):
+        sequence_size = secrets.randbelow(len_test) + 1
+        hex_sequence = (secrets.token_hex(1).encode() for _ in range(sequence_size))
+
+        # Remove all "0a"
+        hex_sequence = (x for x in hex_sequence if x != b"0a")
+
+        yield b" ".join(hex_sequence)
+
+
+def run_commands(data):
     # Run the first executable
     new_result = run(
         [NEW_HEX2RAW],
-        input=test_file.read_bytes(),
+        input=data,
         stdout=PIPE,
         stderr=PIPE,
     )
@@ -24,7 +36,7 @@ def run_commands(test_file):
     # Run the second executable
     old_result = run(
         [OLD_HEX2RAW],
-        input=test_file.read_bytes(),
+        input=data,
         stdout=PIPE,
         stderr=PIPE,
     )
@@ -34,7 +46,7 @@ def run_commands(test_file):
 
 @pytest.mark.parametrize("test_file", get_test_files(Path("tests/same")))
 def test_same_output(test_file):
-    new_result, old_result = run_commands(test_file)
+    new_result, old_result = run_commands(test_file.read_bytes())
 
     # Check if both executables produced the same output
     assert new_result.returncode == old_result.returncode
@@ -44,7 +56,7 @@ def test_same_output(test_file):
 
 @pytest.mark.parametrize("test_file", get_test_files(Path("tests/different")))
 def test_different(test_file):
-    new_result, old_result = run_commands(test_file)
+    new_result, old_result = run_commands(test_file.read_bytes())
 
     assert new_result.returncode == 0
     assert old_result.returncode == 255
@@ -53,7 +65,7 @@ def test_different(test_file):
 
 def test_new_line():
     test_file = Path("tests/test-newline.txt")
-    new_result, old_result = run_commands(test_file)
+    new_result, old_result = run_commands(test_file.read_bytes())
 
     # They both work and return the same thing...
     assert new_result.returncode == old_result.returncode
@@ -62,3 +74,13 @@ def test_new_line():
     # ...but the new hex2raw returns something to stderr while the other one doesn't
     assert len(new_result.stderr) > 0
     assert len(old_result.stderr) == 0
+
+
+@pytest.mark.parametrize("test_data", get_test_data(100, 100))
+def test_random(test_data):
+    new_result, old_result = run_commands(test_data)
+
+    # Check if both executables produced the same output
+    assert new_result.returncode == old_result.returncode
+    assert new_result.stdout == old_result.stdout
+    assert new_result.stderr == old_result.stderr
